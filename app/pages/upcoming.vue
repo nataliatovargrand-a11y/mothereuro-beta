31f47d61e2a2daedf518b8ae2696f8803e51e4fc
Natalia Tovar Grand <nataliatovargrand@Natalia-Tovar-Grand-Mac.local>
2026-02-24 16:58:50 +0100

    FORCE TEST BUILD

--- app/pages/upcoming.vue
+++ app/pages/upcoming.vue
@@ -1,5 +1,8 @@
 <template>
-  <div style="background:black; color:white; padding:200px;">
-    IF YOU SEE THIS, THIS IS THE REAL FILE
+  <div style="background:red;color:white;padding:60px;font-size:40px;">
+    TEST BUILD CONFIRMATION
   </div>
 </template>
+
+<script setup>
+</script>
