bb8b21005dfeb8a1eb898f11bb71ce8657a33eee
Natalia Tovar Grand <nataliatovargrand@Natalia-Tovar-Grand-Mac.local>
2026-02-24 21:27:18 +0100

    Add global typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,18 +8,18 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2026-02-24',
 
+  css: [
+    '@/assets/css/main.css'
+  ],
+
   app: {
     head: {
       title: 'Mother Euro',
       meta: [
-        { name: 'theme-color', content: '#D8D0C6' },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }
+        { name: 'theme-color', content: '#D8D0C6' }
       ],
       link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon-180.png' },
-        { rel: 'manifest', href: '/manifest.json' }
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon-180.png' }
       ]
     }
   }
