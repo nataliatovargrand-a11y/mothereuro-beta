e7a582df6863b1b954d8340fef390d5a600546ac
Natalia Tovar Grand <nataliatovargrand@Natalia-Tovar-Grand-Mac.local>
2026-03-12 18:55:15 +0100

    fix supabase client config

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://hyatwuasoqkjpyulnkqq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5YXR3dWFzb3FranB5dWxua3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MjI5OTgsImV4cCI6MjA4NzM5ODk5OH0.Bbz6x40et8EjdAHQUYe5Ho7wHa17GAejwrJnT-9b_DI'
-)
+const supabaseUrl = 'https://hyatwuasoqkjpyulnkqq.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5YXR3dWFzb3FranB5dWxua3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MjI5OTgsImV4cCI6MjA4NzM5ODk5OH0.Bbz6x40et8EjdAHQUYe5Ho7wHa17GAejwrJnT-9b_DI'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
